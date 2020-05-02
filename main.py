40a01fe36463cfef7004f4698bb0b060d73e6168
OmardTech <omardiaz2012@gmail.com>
2020-05-01 23:58:16 -0700

    prices not working again

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ containers = soup.findAll("a",{"class":"item-title"})
 prices = soup.findAll("li",{"class":"price-current"})
 
 for container in containers:
-        #model = container.findAll("a",{"class":"item-title"})
-        product_model = containers[0].text
+        model = container.findAll("a",{"class":"item-title"})
+        product_model = containers[0].text 
 
 for price in prices:
        #price = prices.find("li",{"class":"price-current"})
