89855c2cd982af5193b3438db30804b6ae024200
omardtech <omardiaz2012@gmail.com>
2020-07-31 00:51:31 -0700

    Prices are seen with model

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 import csv
 
 
-
-
 URL = 'https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709&PageSize=96'
 
 
@@ -14,47 +12,30 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 containers = soup.findAll("div", {"class": "item-cell"})
 
-#model = soup.findAll("div",{"class":"item-info"})
-
-
-
 
 workbook = "products.csv"
 file = open(workbook, "w", newline='')
 
-row = 0 
-col = 0 
+headers = 'Model: , Price: \n'
+
+
+file.write(headers)
+
 
 with file:
-        headers = ['Brand', 'Model', 'Price\n']
-        #writer = csv.DictWriter(file, fieldnames = headers)
 
-        #writer.writeheader()
+    for container in containers:
+        models = []
+        prices = []
+        # print(model_container)
+        product_model = container.find("a", attrs={"class": "item-title"}).text
+        product_price = container.find("li", attrs={"class": "price-current"}).text
+        models.append(product_model)
+        prices.append(product_price)
 
-        for container in containers:
-                        models = []
-                        prices = []
-                        # print(model_container)
-                        product_model = container.find("a", attrs={"class": "item-title"}).text
-                        product_price = container.find("li", attrs={"class": "price-current"}).text
-                        models.append(product_model)
-                        prices.append(product_price)
-           
-                        
-                        
+    file.write("Model: " + product_model + "\n " + "Price: " + product_price)
 
-        
-                        
-                        
-                         
-
-                        #file.write(product_model)
-        #writer.writerow({'Brand' , models})
 print("This is the length of the list :", len(product_model))
-print(models)
-print(prices)
 
 
 file.close()
-
-
