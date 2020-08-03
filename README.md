36363dab3a6c6ada6296a6382c40f31cea96f809
omardtech <omardiaz2012@gmail.com>
2020-08-02 22:10:42 -0700

    Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # computer-part-price-finder
+Web Scraper
+
+
+This searches Newegg.com for the latest GPU prices and organizes them in an Excell Spreadsheet
+
+
+
+##  Demo
+
+
+![Numbers](./screenshot.png "CSV/Excel File Output")
+
+
+
+## Powered by
+
+- [Python3](http://python.org)
+- [Beautifulsoup4](https://pypi.org/project/beautifulsoup4/)
--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import re
 import csv
 
 
-
-#DATE AND TIME 
-today = datetime.now()
-d2 = today.strftime("%B %d, %Y %H:%M")
-
-
 #Website you will be scraping from
 url = "https://www.newegg.com/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7709&PageSize=96"
 
@@ -29,21 +23,32 @@ product_price = soup.find_all("li", attrs={"class": "price-current"})
 #Parsing the data from all the extra html
 
 for a in product_model:
-    models.append(a.getText())
+    models.append(a.getText()[0:70].strip())
 for li in product_price:
     prices.append(li.getText().split('\n')[0].strip()[0:7])
 
 
-#converting list from data to Dictionary
+#with open('products.csv', 'a', newline='') as f:
+    #r = csv.reader(f)
+    #data = [line for line in r]
 
-with open('products.csv', 'a') as f:
-    dictionary = dict(zip(models, prices))
-    field_names = ['Prices:''\n''Models']
-    dict_writer = csv.DictWriter(file, fieldnames=field_names)
-    writer = csv.writer(file)
-    dict_writer.writeheader()
+
+
+
+#converting list from data to Dictionary
+dictionary = dict(zip(models, prices))
+
+
+with open('products.csv','w',newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Model','Prices'])
+    
+    
+    
+   
     
     #writing to CSV file 
     for key, value in dictionary.items():
         writer.writerow([key, value])
+    print('Your results have been printed to a csv file')
 file.close()
